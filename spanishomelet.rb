288f789c187f992bd7f46cb470e801c79f3dcffe
henockbelete <etyeheny@gmail.com>
2017-11-01 15:41:32 +0100

    define ingredients

--- spanishomelet.rb
+++ spanishomelet.rb
@@ -1,1 +1,23 @@
-# Lab work spanish Omelet
+# Lab work spanish Omelet Recipe
+
+def print_divider
+  puts "*" * 40
+  puts "\n"
+end
+
+def print_progress_bar
+  3.times { sleep 0.5; print "." }
+  puts "\n"
+end
+
+
+NUM_POTATOES = 5
+NUM_EGGS = 6
+
+ingredients = [
+  { name: 'potatoes', quantity: NUM_POTATOES },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'onion', quantity: 1 },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
+]
